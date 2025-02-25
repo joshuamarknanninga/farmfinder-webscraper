951e97768c1c204269423ac48e5a49de06a74578
Joshua Nanninga <jmarknanninga@gmail.com>
2025-02-25 11:02:53 -0600

    instlled dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Farm Finder',
+        short_name: 'FarmFinder',
+        theme_color: '#4a90e2'
+      }
+    })
+  ]
 })
